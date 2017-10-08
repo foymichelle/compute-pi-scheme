4a8ad23ba53c038eeb1b9521aa14c3679f2e614f
Michelle Foy <michelle.foy@cenx.com>
2017-10-08 13:08:59 -0400

    Fix distance computation and N increment

--- compute_pi.rkt
+++ compute_pi.rkt
@@ -1,14 +1,23 @@
 #lang racket
 
-(define (compute-pi M N times)
-  (let ([x (random)]
-        [y (random)])
+(define (get-point)
+  (let ((x (random))
+        (y (random)))
+    (cons x (list y))))
+
+(define (distance-from-origin point)
+  (let ((x (first point))
+        (y (second point)))
+    (sqrt (+ (expt x 2) (expt y 2)))))
+
+(define (compute-pi times)
+  (compute-pi-fn 0 0 times))
+
+(define (compute-pi-fn M N times)
+  (let* ([point (get-point)]
+         [distance (distance-from-origin point)])
     (if (> times 0)
-        (let ([distance (/ y x)])
-          (if (< distance 1)
-            (compute-pi (+ M 1) N (- times 1))
-            (compute-pi M (+ 1 N) (- times 1))))
-        (* 4 (/ M N)))))
-  
-  
-  
+        (if (< distance 1)
+            (compute-pi-fn (+ M 1) (+ N 1) (- times 1))
+            (compute-pi-fn M (+ N 1) (- times 1)))
+        (* 4 (real->double-flonum (/ M N))))))
